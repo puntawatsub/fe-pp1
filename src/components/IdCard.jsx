1bd79d303f7ecd687b5fe4d5bb8e27d5db2d0233
Puntawat Subhamani <puntawatsub@gmail.com>
2025-10-29 15:04:20 +0200

    convert date to string using built in method

--- src/components/IdCard.jsx
+++ src/components/IdCard.jsx
@@ -10,81 +10,6 @@ const IdCard = ({
   picture,
   ...rest
 }) => {
-  const dateToString = (date) => {
-    let dayOfTheWeek = "";
-    let monthName = "";
-    switch (date.getDay()) {
-      case 0:
-        dayOfTheWeek = "Sun";
-        break;
-      case 1:
-        dayOfTheWeek = "Mon";
-        break;
-      case 2:
-        dayOfTheWeek = "Tue";
-        break;
-      case 3:
-        dayOfTheWeek = "Wed";
-        break;
-      case 4:
-        dayOfTheWeek = "Thu";
-        break;
-      case 5:
-        dayOfTheWeek = "Fri";
-        break;
-      case 6:
-        dayOfTheWeek = "Sat";
-        break;
-      default:
-        dayOfTheWeek = "Invalid day";
-        break;
-    }
-
-    switch (date.getMonth()) {
-      case 0:
-        monthName = "Jan";
-        break;
-      case 1:
-        monthName = "Feb";
-        break;
-      case 2:
-        monthName = "Mar";
-        break;
-      case 3:
-        monthName = "Apr";
-        break;
-      case 4:
-        monthName = "May";
-        break;
-      case 5:
-        monthName = "Jun";
-        break;
-      case 6:
-        monthName = "Jul";
-        break;
-      case 7:
-        monthName = "Aug";
-        break;
-      case 8:
-        monthName = "Sep";
-        break;
-      case 9:
-        monthName = "Oct";
-        break;
-      case 10:
-        monthName = "Nov";
-        break;
-      case 11:
-        monthName = "Dec";
-        break;
-      default:
-        monthName = "Invalid month";
-        break;
-    }
-
-    return `${dayOfTheWeek} ${monthName} ${date.getDate()} ${date.getFullYear()}`;
-  };
-
   return (
     <div {...rest} className="card">
       <img className="image" src={picture}></img>
@@ -108,7 +33,7 @@ const IdCard = ({
         </div>
         <div className="idContent">
           <a>Birth:</a>
-          {dateToString(birth)}
+          {birth.toDateString()}
         </div>
       </div>
     </div>
